4ab41132c425dc3a48795a8a368e4b0fdc48f803
thegleam <thegleam.tech@gmail.com>
2020-08-30 20:52:34 +0100

    Updates

---
+++ precache-manifest.d3317f141070e7955cd5f8713ef0d319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f747e1abe177ada55541481c1fcbe471",
+    "url": "/qa-app/index.html"
+  },
+  {
+    "revision": "a287ea4b2e40992b1ebd",
+    "url": "/qa-app/static/css/main.7842c253.chunk.css"
+  },
+  {
+    "revision": "c25c5c4da1bd1e82ea0b",
+    "url": "/qa-app/static/js/2.218133da.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/qa-app/static/js/2.218133da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a287ea4b2e40992b1ebd",
+    "url": "/qa-app/static/js/main.93f7f59e.chunk.js"
+  },
+  {
+    "revision": "eebd8a858038d1fcabdf",
+    "url": "/qa-app/static/js/runtime-main.c75d631b.js"
+  }
+]);
